35489079d984310fee2a85af292224fff51c235d
Tommy Webb <tommy@calyxinstitute.org>
2024-06-25 14:05:44 +0000

    Add Strict Location Redaction setting
    Issue: calyxos#2468
    Change-Id: Ic6bb59a32fe9441ecc8488c97c80e1ecbb51d5a1

--- src/com/android/settings/SettingsApplication.java
+++ src/com/android/settings/SettingsApplication.java
@@ -42,6 +42,7 @@ import com.android.settings.metrics.SettingsMetricsLogger;
 import com.android.settings.msds.MSDLPlayerWrapper;
 import com.android.settings.overlay.FeatureFactory;
 import com.android.settings.overlay.FeatureFactoryImpl;
+import com.android.settings.privacy.StrictLocationRedactionReceiver;
 import com.android.settings.spa.SettingsSpaEnvironment;
 import com.android.settingslib.applications.AppIconCacheManager;
 import com.android.settingslib.datastore.BackupRestoreStorageManager;
@@ -108,6 +109,8 @@ public class SettingsApplication extends Application {
         }
 
         registerActivityLifecycleCallbacks(new DeveloperOptionsActivityLifecycle());
+
+        new StrictLocationRedactionReceiver().register(this);
 
         if (Flags.msdlFeedback()) {
             MSDLPlayerWrapper.INSTANCE.createPlayer(this);
